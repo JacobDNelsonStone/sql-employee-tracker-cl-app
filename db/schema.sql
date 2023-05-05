7092c7611c50081b131a0001f9211135acbbed54
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-05-05 11:04:33 -0500

    blah

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,17 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  
-)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NOT NULL,
+  manager_id INT
+);
 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department( dept_name )
+VALUES ('Finance'),
+       ('Legal'),
+       ('Sales'),
+       ('Engineering')
+       ('HR'); 
