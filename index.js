395dc440072655add9355660cab5002999a7d8fc
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-05-07 18:21:57 -0500

    working on main functions

--- index.js
+++ index.js
@@ -74,3 +74,82 @@ function viewDepartments(){
     mainQuestions();
   });
 };
+
+function viewRoles(){
+  db.query('SELECT * FROM roles', function (err, res) {
+    if (err){
+      console.log(err);
+      mainQuestions();
+    }
+    console.log(res);
+    console.table(res);
+    mainQuestions();
+  });
+};
+
+function viewEmployees(){
+  db.query('SELECT * FROM employee', function (err, res){
+    if (err){
+      console.log(err);
+      mainQuestions();
+    }
+    console.log(res);
+    console.table(res);
+    mainQuestions();
+  });
+};
+
+function addDepartment() {
+
+  inquirer.prompt({
+    type: 'input',
+    message: 'Please enter the name for the new department',
+    name: 'newDept'
+  }).then(function(answer) {
+  
+  db.query('INSERT INTO department (dept_name) VALUES (?)', [answer.newDept], function (err, res) {
+    if (err){
+      console.log(err);
+      mainQuestions();
+    };
+    console.log(res);
+    console.table(res);
+    mainQuestions();
+  });
+ })
+};
+function addRole(){
+
+  inquirer.prompt([
+    {
+      title: 'input',
+      message: 'please enter the ID number for the new role',
+      name: 'newRoleId'
+    },
+    {
+      type: 'input',
+      message: 'Please enter the title of the role you would like to add',
+      name: 'newRoleTitle'
+    },
+    {
+      type: 'input',
+      message: 'Please enter the salary of the new role',
+      name: 'newRoleSalary'
+    },
+    {
+      type: 'input',
+      message: 'Please enter the ID number of the department this role belong',
+      name: 'newRoleDeptId'
+    }
+  ]).then(function (answer){
+  db.query('INSERT INTO role (id, title, salary, department_id ) VALUES (?, ?, ?)', [answer.newRoleId], [answer.newRoleTitle], [answer.newRoleSalary], [answer.newRoleDeptId], function (err, res){
+    if(err){
+      console.log(err);
+
+    }
+    console.log(res);
+    console.table(res);
+    mainQuestions();
+  })
+});
+};
